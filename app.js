8335c7d1439ae13a4e9735aa7989b04e28b8ca22
shellus <shellus@endaosi.com>
2016-04-14 00:10:03 +0800

    init

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var scraperjs = require('scraperjs');
-scraperjs.DynamicScraper.create('http://www.woshipm.com/')
+var path = require('path');
+var request = require('request');
+var fs = require('fs');
+
+const url = 'http://www.xieell.com/';
+const filter_prefix = "http://xieell.21huangye.com/uploads/";
+const storage_dir = 'storage/image/';
+
+!fs.existsSync(storage_dir) && fs.mkdirSync(storage_dir) && console.log("create path: " + path.resolve(storage_dir));
+
+scraperjs.StaticScraper.create(url)
     .scrape(function($) {
-        return $(".list-h3 a").map(function() {
-            return $(this).text();
+        return $("img").map(function() {
+            return $(this).attr('src');
         }).get();
     })
     .then(function(news) {
-        console.log(news);
-    }, function (err) {
-        console.log(err);
+        news = news.filter(function (v) {
+            return v.substr(0,filter_prefix.length) === filter_prefix;
+        });
+        news.map(function (v) {
+            request(v).pipe(fs.createWriteStream(storage_dir + path.basename(v)));
+            console.dir(v);
+        });
     });
